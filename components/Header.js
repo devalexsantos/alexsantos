6af21042d3d7f87a733eaf724d7e3e643b591b14
Alex Santos <devalexsantos@gmail.com>
2022-05-22 13:17:04 -0300

    corrigindo bug de links

--- components/Header.js
+++ components/Header.js
@@ -35,15 +35,18 @@ export default function Header(props) {
                         <Link
                           key={item.name}
                           href={item.href}
-                          className={classNames(
-                            item.current
-                              ? "bg-zinc-900 text-white"
-                              : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                            "px-3 py-2 rounded-md text-sm font-medium"
-                          )}
                           aria-current={item.current ? "page" : undefined}
                         >
-                          {item.name}
+                          <a
+                            className={classNames(
+                              item.current
+                                ? "bg-zinc-900 text-white"
+                                : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                              "px-3 py-2 rounded-md text-sm font-medium"
+                            )}
+                          >
+                            {item.name}
+                          </a>
                         </Link>
                       ))}
                     </div>
