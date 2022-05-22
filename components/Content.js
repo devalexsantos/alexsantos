cf40d69b8bd5a2d83c9cf6bf1ca873ba8f75e559
Alex Santos <devalexsantos@gmail.com>
2022-05-22 15:21:03 -0300

    criando estilo dos posts

--- components/Content.js
+++ components/Content.js
@@ -4,9 +4,9 @@ export default function Content(props) {
       <div className="pb-4">
         <h2>Últimas Publicações:</h2>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full">
         {props.posts.map((item, index) => (
-          <div key={index}>
+          <div key={index} className="bg-zinc-900 m-4 p-4 rounded shadow-md">
             <span className="text-xl items-center">{item.tittle} </span>
             <span className="text-md">
               - {item.date.substr(0, 10).split("-").reverse().join("/")}
